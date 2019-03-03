a6ba0a0d322461806ab0ef1b2257e53e59b1f94c
Thomas Rayden <thomasmwrayden@gmail.com>
2019-03-03 17:51:16 +0800

    Added jest-extended

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ['jest-extended'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  }
 };
