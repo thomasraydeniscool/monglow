9480e5c280e59254c50b6e255d8d4c55b9f6f11f
Thomas Rayden <thomasmwrayden@gmail.com>
2019-03-13 16:57:03 +0800

    Provide MongoDB driver properties

--- lib/index.ts
+++ lib/index.ts
@@ -1,9 +1,7 @@
+import { ObjectId, Timestamp, MinKey, MaxKey, DBRef, Long } from 'mongodb';
 import Monglow from './Monglow';
 import Model from './Model';
 
-export {
-  Monglow,
-  Model
-}
+export { Monglow, Model, ObjectId, Timestamp, MinKey, MaxKey, DBRef, Long };
 
 export default Monglow;
