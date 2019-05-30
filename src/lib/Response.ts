5dacf351fd6d086bd142071f59786226797fc6e1
Thomas Rayden <thomasmwrayden@gmail.com>
2019-05-30 14:17:18 +0800

    Remove the need for type casting

--- src/lib/Response.ts
+++ src/lib/Response.ts
@@ -2,6 +2,18 @@ import ow from 'ow';
 import MonglowDocument from './Document';
 
 class MonglowResponse {
+  public static async create(
+    task: Promise<any>,
+    wrap: false
+  ): Promise<MonglowDocument>;
+  public static async create(
+    task: Promise<any>,
+    wrap: true
+  ): Promise<MonglowResponse>;
+  public static async create(
+    task: Promise<any[]>,
+    wrap: false
+  ): Promise<MonglowResponse>;
   public static async create(task: Promise<any>, wrap = false) {
     ow(task, ow.promise);
     const data = await task;
